e44298c303922eee56dabb4cd40886212a0f15e8
kris <29414@qq.com>
2019-09-22 14:05:43 +0800

    add glog

--- log.go
+++ log.go
@@ -106,28 +106,58 @@ func format(v ...interface{}) string {
 	}
 	return s
 }
+func logerformat(v ...interface{}) string {
+	_, file, line, ok := runtime.Caller(3)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+	s := fmt.Sprint(v...)
+	if !code {
+		return s
+	}
+	if gpath != "" {
+		//过滤路径
+		pos := strings.Index(file, gpath)
+		if pos != -1 {
+			pos += len(gpath)
+		} else {
+			pos = 0
+		}
+		return fmt.Sprintf("%s:%d %s", file[pos+1:], line, s)
+	}
+	return s
+}
+
 
 func Debug(v ...interface{}) { log.Debug(format(v...)) }
-
+func Debuglogger(v ...interface{}) { log.Debug(logerformat(v...)) }
 func Debugf(s string, v ...interface{}) { log.Debugf(s, format(v...)) }
 
 func Info(v ...interface{}) { log.Info(format(v...)) }
+func Infologger(v ...interface{}) { log.Info(logerformat(v...)) }
 
 func Infof(s string, v ...interface{}) { log.Infof(s, format(v...)) }
 
 func Notice(v ...interface{}) { log.Notice(format(v...)) }
+func Noticelogger(v ...interface{}) { log.Notice(logerformat(v...)) }
 
 func Warn(v ...interface{}) { log.Warn(format(v...)) }
+func Warnlogger(v ...interface{}) { log.Warn(logerformat(v...)) }
 
 func Warnf(s string, v ...interface{}) { log.Warnf(s, format(v...)) }
 
 func Error(v ...interface{}) { log.Error(format(v...)) }
+func Errorlogger(v ...interface{}) { log.Error(logerformat(v...)) }
 
 func Errorf(s string, v ...interface{}) { log.Errorf(s, format(v...)) }
 
 func Panic(v ...interface{}) { log.Panic(format(v...)) }
+func Paniclogger(v ...interface{}) { log.Panic(logerformat(v...)) }
 
 func Alert(v ...interface{}) { log.Alert(format(v...)) }
+func Alertlogger(v ...interface{}) { log.Alert(logerformat(v...)) }
 
 func Fatal(v ...interface{}) { log.Fatal(format(v...)) }
+func Fatallogger(v ...interface{}) { log.Fatal(logerformat(v...)) }
 
